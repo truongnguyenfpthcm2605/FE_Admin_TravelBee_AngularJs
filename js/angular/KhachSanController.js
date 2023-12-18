11ce042bfb52bb9c908873c984f7783ebd0b6ede
VanThien2504 <99249879+VanThien2504@users.noreply.github.com>
2023-12-18 23:45:55 +0700

    update_CRUDHotel

--- js/angular/KhachSanController.js
+++ js/angular/KhachSanController.js
@@ -6,93 +6,19 @@ app.controller("KhachSanController", function ($scope, $location, $http, $rootSc
                 'Authorization': 'Bearer ' + $rootScope.token
             }
         }).then(function (response) {
-            $rootScope.hotel = response.data
-            $scope.current = $rootScope.hotel
-            console.log($rootScope.hotel)
+            $rootScope.hotelParam = response.data
+            $scope.current = $rootScope.hotelParam
         })
             .catch(function (error) {
 
             });
-
     }
     $scope.findAll()
 
-    $scope.save = function () {
-        Swal.fire({
-            title: "Bạn có muốn lưu khách sạn này ?",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, save it!"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                $http.delete($rootScope.url + "/api/v1/hotel/save", {
-                    headers: {
-                        'Authorization': 'Bearer ' + $rootScope.token
-                    }
-                }).then(function (response) {
-                    Swal.fire({
-                        title: "Deleted!",
-                        text: "Khách sạn đã được lưu thành công.",
-                        icon: "success"
-                    });
-                    // $scope.removeElementById(id)
-                    // $scope.current = $rootScope.hotel
-                    $scope.findAll()
-                })
-                    .catch(function (error) {
-                        console.log(error)
-                        Swal.fire({
-                            icon: 'error',
-                            title: 'thêm khách sạn thất bại',
-                            text: 'kiểm tra lai dữ liệu',
-                        });
-                    });
 
-            }
-        });
-    }
-    $scope.updateid = function (id) {
-        Swal.fire({
-            title: "Bạn có muốn chỉnh sửa khách sạn này ?",
-            text: "sẽ không thể phục hồi lại được!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, update it!"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                $http.delete($rootScope.url + "/api/v1/hotel/update/" + id, {
-                    headers: {
-                        'Authorization': 'Bearer ' + $rootScope.token
-                    }
-                }).then(function (response) {
-                    Swal.fire({
-                        title: "Deleted!",
-                        text: "Khách sạn đã được update thành công.",
-                        icon: "success"
-                    });
-                    $scope.removeElementById(id)
-                    $scope.current = $rootScope.hotel
-                    $scope.findAll()
-                })
-                    .catch(function (error) {
-                        console.log(error)
-                        Swal.fire({
-                            icon: 'error',
-                            title: 'xóa phương tiện thất bại',
-                            text: 'kiểm tra lai dữ liệu',
-                        });
-                    });
-
-            }
-        });
-    }
     $scope.deleteid = function (id) {
         Swal.fire({
-            title: "Bạn có muốn xóa khách sạn này ?",
+            title: "Bạn có muốn xóa khách này ?",
             text: "sẽ không thể phục hồi lại được!",
             icon: "warning",
             showCancelButton: true,
@@ -112,14 +38,13 @@ app.controller("KhachSanController", function ($scope, $location, $http, $rootSc
                         icon: "success"
                     });
                     $scope.removeElementById(id)
-                    $scope.current = $rootScope.hotel
-                    $scope.findAll()
+                    $scope.current = $rootScope.hotelParam
                 })
                     .catch(function (error) {
                         console.log(error)
                         Swal.fire({
                             icon: 'error',
-                            title: 'xóa phương tiện thất bại',
+                            title: 'xóa khách sạn thất bại',
                             text: 'kiểm tra lai dữ liệu',
                         });
                     });
@@ -130,22 +55,22 @@ app.controller("KhachSanController", function ($scope, $location, $http, $rootSc
 
     $scope.searchKeyword = function (keyword) {
         const searchInput = keyword || '';
-        $scope.current = $rootScope.voucherParam.filter(function (voucher) {
-            return voucher.id.toLowerCase().indexOf(searchInput.toLowerCase()) !== -1 ||
-                voucher.title.toLowerCase().indexOf(searchInput.toLowerCase()) !== -1;
+        $scope.current = $rootScope.hotelParam.filter(function (hotel) {
+            return hotel.id.toLowerCase().indexOf(searchInput.toLowerCase()) !== -1 ||
+                hotel.title.toLowerCase().indexOf(searchInput.toLowerCase()) !== -1;
         });
     };
-    $scope.exportExcel = function () {
+    $scope.exportExcel = function(){
         let table2excel = new Table2Excel();
-        table2excel.export(document.querySelector("#table-pt"), 'Danh Sách phương tiện');
+        table2excel.export(document.querySelector("#table-hotel"),'Danh Sách hotel');
     }
     $scope.removeElementById = function (id) {
-        $rootScope.voucherParam = $rootScope.voucherParam.filter(function (item) {
+        $rootScope.hotelParam = $rootScope.hotelParam.filter(function (item) {
             return item.id !== id;
         });
     };
 
-
+    
 
 
 
